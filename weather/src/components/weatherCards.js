58bcf2ce97f0586324610b9ada8fc9cf19bf42f2
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-02-23 13:14:29 +0000

    Initial sound effects support via use-sound package

--- weather/src/components/weatherCards.js
+++ weather/src/components/weatherCards.js
@@ -1,11 +1,11 @@
 import WeatherCard from './weatherCard';
 import './weatherCards.css' ;
 
-export default function WeatherCards({data}) {
+export default function WeatherCards({data, playSound}) {
   return (
     <div className="my-weather-cards justify-content-center">
 			{data.map(dayData => 
-				<WeatherCard data={dayData} key={dayData.periods[0].dt}/>
+				<WeatherCard data={dayData} key={dayData.periods[0].dt} playSound={playSound} />
 			)}
 		</div>
   );
